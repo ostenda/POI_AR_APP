65faab1136a840119733327b93fe271ee8efee35
ostenda <ostenda95@gmail.com>
2023-05-06 21:32:58 +0100

    task 1

--- app/src/main/java/com/example/com527_michalostenda/MainActivity.kt
+++ app/src/main/java/com/example/com527_michalostenda/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.com527_michalostenda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import org.osmdroid.config.Configuration
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // This line sets the user agent, a requirement to download OSM maps
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+
         setContentView(R.layout.activity_main)
+        val map1 = findViewById<MapView>(R.id.map1)
+        map1.controller.setZoom(14.0)
+        map1.controller.setCenter(GeoPoint(51.05, -0.72))
+
     }
 }
