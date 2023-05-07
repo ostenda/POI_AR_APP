96d7ccda57781e2fe429a31bc4165b9f5cce04cc
ostenda <ostenda95@gmail.com>
2023-05-07 21:30:54 +0100

    start of task 2

--- app/src/main/java/com/example/com527_michalostenda/MainActivity.kt
+++ app/src/main/java/com/example/com527_michalostenda/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.example.com527_michalostenda
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import org.osmdroid.config.Configuration
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.ItemizedIconOverlay
+import org.osmdroid.views.overlay.OverlayItem
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
+
+    lateinit var map1: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -15,9 +33,95 @@ class MainActivity : AppCompatActivity() {
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
 
         setContentView(R.layout.activity_main)
-        val map1 = findViewById<MapView>(R.id.map1)
-        map1.controller.setZoom(14.0)
+        map1 = findViewById<MapView>(R.id.map1)
+        map1.controller.setZoom(16.0)
         map1.controller.setCenter(GeoPoint(51.05, -0.72))
+
+        requestPermissions()
+
+        val markerGestureListener = object:ItemizedIconOverlay.OnItemGestureListener<OverlayItem>
+        {
+            override fun onItemLongPress(i: Int, item:OverlayItem ) : Boolean
+            {
+                Toast.makeText(this@MainActivity, item.snippet, Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            override fun onItemSingleTapUp(i: Int, item:OverlayItem): Boolean
+            {
+                Toast.makeText(this@MainActivity, item.snippet, Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+        val items = ItemizedIconOverlay(this, arrayListOf<OverlayItem>(), markerGestureListener)
+        val fernhurst = OverlayItem("Fernhurst", "Village in West Sussex", GeoPoint(51.05, -0.72))
+        val blackdown = OverlayItem("Blackdown", "highest point in West Sussex", GeoPoint(51.0581, -0.6897))
+        items.addItem(fernhurst)
+        items.addItem(blackdown)
+        map1.overlays.add(items)
+    }
+
+    fun POI_click() {
+
+
+    }
+
+
+    fun requestPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                0
+            )
+        } else {
+            startGps()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            0 -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startGps()
+                } else {
+                    AlertDialog.Builder(this).setPositiveButton("OK", null)
+                        .setMessage("GPS will not work as you have denied access").show()
+                }
+            }
+        }
+    }
+
+    // Suppress lint check (sanity check) about missing permission
+    // We check permissions above, so don't need to do it here
+    @SuppressLint("MissingPermission")
+    fun startGps() {
+        val mgr = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        mgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0f, this)
+    }
+
+    override fun onLocationChanged(loc: Location) {
+        map1.controller.setCenter(GeoPoint(loc.latitude, loc.longitude))
+    }
+
+    override fun onProviderEnabled(provider: String) {
+
+    }
+
+    override fun onProviderDisabled(provider: String) {
+
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
 
     }
 }
